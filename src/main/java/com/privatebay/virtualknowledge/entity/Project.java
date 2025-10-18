baf916cc7af8682c315b1bbc2c32d002b680fce4
josrojrom1 <josrojrom@alum.us.es>
2025-10-18 20:15:12 +0200

    Project entity, relationship with User, added all anotations for all attributes

--- src/main/java/com/privatebay/virtualknowledge/entity/Project.java
+++ src/main/java/com/privatebay/virtualknowledge/entity/Project.java
@@ -2,8 +2,12 @@ package com.privatebay.virtualknowledge.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -12,13 +16,17 @@ import jakarta.persistence.Table;
 @Table(name = "projects")
 public class Project {
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(nullable = false, length = 32)
 	private String name;
 	
+    @Column(nullable = false, length = 300, unique = true)
 	private String description;
 	
+    @Column(name = "creatio_date", nullable = false, updatable = false)
 	private LocalDateTime creationDate;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
