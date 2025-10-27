ab84ac5cb25e42f43a530f97d3cfeafc4185eaaf
josrojrom1 <josrojrom@alum.us.es>
2025-10-27 19:01:24 +0100

    Cleanup main app

--- src/main/java/com/privatebay/virtualknowledge/VirtualknowledgeApplication.java
+++ src/main/java/com/privatebay/virtualknowledge/VirtualknowledgeApplication.java
@@ -8,14 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class VirtualknowledgeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VirtualknowledgeApplication.class, args);
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String rawPassword = "12345"; // 👈 Contraseña que usarás en Postman
-		String hashedPassword = encoder.encode(rawPassword);
-
-		System.out.println("Contraseña en texto plano: " + rawPassword);
-		System.out.println("HASH GENERADO: " + hashedPassword);
 
 	}
 
