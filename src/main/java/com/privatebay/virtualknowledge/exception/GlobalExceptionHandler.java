e0ce1d6a5352c8157ef9f5629f805d45b13a73ea
josrojrom1 <josrojrom@alum.us.es>
2026-06-30 15:54:46 +0200

    core(exception): New ErrorResponse and GlobalException files in exception packet in order to centralice exceptions. Pending to separate error intercceptor from auth interceptor, not needed yet.

---
+++ src/main/java/com/privatebay/virtualknowledge/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.privatebay.virtualknowledge.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<ErrorResponse> handleAuthException(AuthenticationException ex) {
+		ErrorResponse error = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(),
+				"Credenciales inválidas", "El email o la contraseña son incorrectos");
+		return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorResponse> handleGeneralException(RuntimeException ex) {
+		ErrorResponse error = new ErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
+				"Error en la operación", ex.getMessage());
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+}
