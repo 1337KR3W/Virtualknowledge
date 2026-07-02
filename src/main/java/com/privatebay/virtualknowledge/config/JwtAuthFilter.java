452a8dafc4c9532553f54dfe92862f4423f59e78
josrojrom1 <josrojrom@alum.us.es>
2026-07-03 00:30:58 +0200

    feature(Jwt): Updated Jwt filter and deleted WebConfig

--- src/main/java/com/privatebay/virtualknowledge/config/JwtAuthFilter.java
+++ src/main/java/com/privatebay/virtualknowledge/config/JwtAuthFilter.java
@@ -4,10 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,8 +14,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.privatebay.virtualknowledge.service.JwtService;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -50,12 +46,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 			if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 				UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-				List<String> roles = jwtService.extractRoles(jwt);
-				List<SimpleGrantedAuthority> authorities = roles.stream().map(SimpleGrantedAuthority::new)
-						.collect(Collectors.toList());
-
 				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
-						null, authorities);
+						null, userDetails.getAuthorities());
 
 				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
