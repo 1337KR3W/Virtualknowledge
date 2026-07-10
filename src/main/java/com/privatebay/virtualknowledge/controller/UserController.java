f7e738416405551925a16ca7023152e3e2f24ecb
josrojrom1 <josrojrom@alum.us.es>
2026-07-10 03:37:44 +0200

    feature(swagger): Changed to version 2.8.x in pom.xml for compatibility reasons. Started documenting user-controller endpoints

--- src/main/java/com/privatebay/virtualknowledge/controller/UserController.java
+++ src/main/java/com/privatebay/virtualknowledge/controller/UserController.java
@@ -3,13 +3,14 @@ package com.privatebay.virtualknowledge.controller;
 import com.privatebay.virtualknowledge.dto.UserRequestDTO;
 import com.privatebay.virtualknowledge.dto.UserResponseDTO;
 import com.privatebay.virtualknowledge.service.UserService;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:4200")
+@Tag(name = "Users", description= "Endpoints for users management")
 public class UserController {
 
     private final UserService userService;
@@ -25,11 +27,19 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Obtain user profile", description = "Returns user details providing an ID")
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "200", description = "User not found"),
+    		@ApiResponse(responseCode = "404", description = "User not found")
+    		
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDTO> getProfile(@PathVariable Long userId) {
         return ResponseEntity.ok(userService.findById(userId));
     }
 
+    
+    @Operation(summary = "Register new user", description = "Only admins can register new users")
     @PostMapping("/admin/register")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Map<String, String>> registerByAdmin(@Valid @RequestBody UserRequestDTO request) {
@@ -39,18 +49,21 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary =  "List users by department", description = "Only admins can list users by department")
     @GetMapping("/admin/department/{departmentId}")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<UserResponseDTO>> getUsersByDepartment(@PathVariable Long departmentId) {
         return ResponseEntity.ok(userService.findByDepartmentId(departmentId));
     }
 
+    @Operation(summary = "List all users", description = "Only admins can list all users")
     @GetMapping("/admin/all")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.findAll());
     }
     
+    @Operation(summary = "Update user", description = "Only admins can update users")
 	@PutMapping("/admin/edit/{id}")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id,
@@ -58,6 +71,7 @@ public class UserController {
 		return ResponseEntity.ok(userService.updateUser(id, request));
 	}
     
+    @Operation(summary = "Delete user", description = "Only admins can delete users")
 	@DeleteMapping("/admin/delete/{id}")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
