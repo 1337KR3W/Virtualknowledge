ddfb6ea668952141a06d2d2d1bb23a9e12472d2c
josrojrom1 <josrojrom@alum.us.es>
2026-07-10 02:03:41 +0200

    feature(timesheet mapper): Updated TimeSheetMaper with new one using MapStruct and deleted old one

--- src/main/java/com/privatebay/virtualknowledge/mapper/TimeSheetMapper.java
+++ src/main/java/com/privatebay/virtualknowledge/mapper/TimeSheetMapper.java
@@ -2,17 +2,39 @@ package com.privatebay.virtualknowledge.mapper;
 
 import com.privatebay.virtualknowledge.dto.*;
 import com.privatebay.virtualknowledge.entity.TimeSheet;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-@Component
-public class TimeSheetMapper {
+@Mapper(componentModel = "spring")
+public interface TimeSheetMapper {
 
-	public TimeSheetRequestDTO toDTO(Long userId, String weekId, String globalComment, List<TimeSheet> entries) {
+	@Mapping(target = "rows", expression = "java(mapEntriesToRows(entries))")
+	@Mapping(target = "weekDates", expression = "java(calculateWeekDates(weekId))")
+	TimeSheetRequestDTO toDTO(Long userId, String weekId, String globalComment, List<TimeSheet> entries);
+
+	default List<LocalDate> calculateWeekDates(String weekId) {
+		if (weekId == null)
+			return new ArrayList<>();
+
+		LocalDate monday = LocalDate.parse(weekId + "-1", DateTimeFormatter.ISO_WEEK_DATE);
+
+		return IntStream.range(0, 7).mapToObj(monday::plusDays).collect(Collectors.toList());
+	}
+
+	default List<ProjectTimeRowDTO> mapEntriesToRows(List<TimeSheet> entries) {
+		if (entries == null)
+			return new ArrayList<>();
+
 		Map<Long, ProjectTimeRowDTO> rowsMap = new HashMap<>();
 
 		for (TimeSheet ts : entries) {
 			Long pid = ts.getProject().getId();
+
 			rowsMap.putIfAbsent(pid,
 					new ProjectTimeRowDTO(pid, ts.getProject().getName(), ts.getProject().getDepartment().getName()));
 
@@ -21,11 +43,6 @@ public class TimeSheetMapper {
 			rowsMap.get(pid).addEntry(dayKey, entry);
 		}
 
-		TimeSheetRequestDTO dto = new TimeSheetRequestDTO();
-		dto.setWeekId(weekId);
-		dto.setUserId(userId);
-		dto.setGlobalComment(globalComment);
-		dto.setRows(new ArrayList<>(rowsMap.values()));
-		return dto;
+		return new ArrayList<>(rowsMap.values());
 	}
 }
